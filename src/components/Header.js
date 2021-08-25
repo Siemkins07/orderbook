242e1129e40d28df523b61d2a3e9b512402db72e
Jacek <jacek.pobiarzyn@looksoft.pl>
2021-08-25 23:10:07 +0200

    add baseurl to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,17 @@ import React from 'react'
 import logo from '../assets/bitbay-logo.svg'
 
 const Header = () => {
+const baseUrl = 'https://siemkins07.github.io/orderbook/'
+
     return (
         <header>
-            <a href="/"> <img src={logo} alt='logo'></img></a>
+            <a href={baseUrl}> <img src={logo} alt='logo'></img></a>
            
             <nav>
                 <ul>
-                    <li><a href='/'>BitBay One</a></li>
-                    <li><a href='/'>BitBay Two</a></li>
-                    <li><a href='/'>BitBay Tree</a></li>
+                    <li><a href={baseUrl}>BitBay One</a></li>
+                    <li><a href={baseUrl}>BitBay Two</a></li>
+                    <li><a href={baseUrl}>BitBay Tree</a></li>
                 </ul>
             </nav>
         </header>
